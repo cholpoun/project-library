913f221acb4ecf01c9fb4e15e23be07ba18a7f63
Cholpon <127111343+cholpoun@users.noreply.github.com>
2024-09-19 20:22:38 +0200

    movie cards working and updated

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 /*Here we have created two different arrays that you can work with if you want.
 If you choose to create your own arrays with elements, just make sure that some
 of the properties make sense to filter on, and some to sort on.*/
+// console.log('movie time')
 
-const movies = [
+const MOVIES = [
   {
     title: 'The Great Gatsby',
     director: 'F. Scott Fitzgerald',
     year: 1925,
     genre: 'Fiction',
     rating: 4.2,
-    runtime: 145,
     description:
       'A classic novel set in the Roaring Twenties, it explores themes of wealth, love, and the American Dream through the enigmatic Jay Gatsby.',
-    image: './books-images/the-great-gatsby.jpg'
+    img: './books-images/the-great-gatsby.jpg'
   },
 
   {
     title: 'The Proposal',
     director: 'Anne Fletcher',
     year: 2009,
-    genre: 'Romantic Comedy',
+    genre: 'Comedy',
     rating: 6.8,
-    runtime: 108,
     description:
       'The Proposal is the story of a tough editor Margaret Tate (Bullock) who forces her assistant Andrew Tate (Ryan Reynolds) to marry her so she can stay in the U.S.',
-    image: './books-images/the-great-gatsby.jpg'
+    img: './books-images/the-great-gatsby.jpg'
   },
 
   {
@@ -33,22 +32,20 @@ const movies = [
     year: 2022,
     genre: 'Fiction',
     rating: 4.2,
-    runtime: 145,
     description:
       'A classic novel set in the Roaring Twenties, it explores themes of wealth, love, and the American Dream through the enigmatic Jay Gatsby.',
-    image: './books-images/the-great-gatsby.jpg'
+    img: './books-images/the-great-gatsby.jpg'
   },
 
   {
     title: 'The Fish',
     director: 'Anne Fletcher',
     year: 2019,
-    genre: 'Romantic Comedy',
+    genre: 'Comedy',
     rating: 7.2,
-    runtime: 135,
     description:
       'A classic novel set in the Roaring Twenties, it explores themes of wealth, love, and the American Dream through the enigmatic Jay Gatsby.',
-    image: './books-images/the-great-gatsby.jpg'
+    img: './books-images/the-great-gatsby.jpg'
   },
 
   {
@@ -57,10 +54,9 @@ const movies = [
     year: 2011,
     genre: 'Drama',
     rating: 0.2,
-    runtime: 115,
     description:
       'A classic novel set in the Roaring Twenties, it explores themes of wealth, love, and the American Dream through the enigmatic Jay Gatsby.',
-    image: './books-images/the-great-gatsby.jpg'
+    img: './books-images/the-great-gatsby.jpg'
   },
 
   {
@@ -69,70 +65,29 @@ const movies = [
     year: 2019,
     genre: 'Comedy',
     rating: 7.2,
-    runtime: 125,
     description:
       'A classic novel set in the Roaring Twenties, it explores themes of wealth, love, and the American Dream through the enigmatic Jay Gatsby.',
-    image: './books-images/the-great-gatsby.jpg'
+    img: './books-images/the-great-gatsby.jpg'
   },
 ]
 
+const container = document.getElementById('container')
+// const favourites = document.getElementById('favourites')
+// const filterDropdown = document.getElementById('filterDropdown')
 
-/*
-const loadMovies = (movies) => {
-  movieCard.innerHTML = ""
-
-  // Create card that should be inserted
-  movies.forEach((movies) => {
-    movieCard.innerHTML +=
-      `
-    <div class="movie-card"> 
-    <h3 class="title"> ${movies.title} </h3>
-    <img class="card-img" src=${movies.image}>
-    <p> Written by ${movies.director} </p>
-    <br>
-    <p> <strong>Genre:</strong> ${movies.genre} </p>
-    <p> <strong>Published:</strong> ${movies.year} </p>
-    <p> <strong>Rating:</strong> ${movies.rating} </p>
-    <br>
-    <p> ${movies.description} </p>
-    <br>
-    </div> 
-    `
+const getMovies = (moviesArray) => {
+  container.innerHTML = ''
+  moviesArray.forEach(movie => {
+    container.innerHTML += `
+      <div class="card">
+      <h3>${movie.title}</h3>
+      <img src=${movie.img} alt=${movie.name} />
+      <p>Director: ${movie.director}, Year: ${movie.year}</p>
+      <p>Genre: ${movie.genre}, Rating: ${movie.rating}</p>
+      <p>${movie.description}</p>
+      </div>`
   })
 }
 
-loadMovies();
+getMovies(MOVIES);
 
-*/
-
-window.onload = function () {
-  displayMovies(movies); // Display all movies when the page loads
-};
-
-function displayMovies(movieArray) {
-  const moviesContainer = document.getElementById('movie-list');
-  movieContainer.innerHTML = ''; // Clear any previous content
-
-  movieArray.forEach(movie => {
-    const movieElement = document.createElement('div');
-    movieElement.innerHTML = `${movies.title} (${movies.year}) - Rating: ${movies.rating}`;
-    movieContainer.appendChild(movieElement);
-  });
-}
-
-/*
-loadMovies();
-
-window.onload = function() {
-  displayMovies(movies);  // Display all movies when the page loads
-};
-function displayMovies(movieArray) {
-  const loadMovies = document.getElementById('book-list');
-  movies.innerHTML = '';  // Clear any previous content
-  movies.forEach(book => {
-    const bookElement = document.createElement('div');
-    bookElement.innerHTML = `${movies.title} (${movies.year}) - Rating: ${book.rating}`;
-    bookContainer.appendChild(bookElement);
-  });
-}
-*/
